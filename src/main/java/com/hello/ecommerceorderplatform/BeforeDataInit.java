e61f665bb827b4766c7833949d187ba649b3f7be
BackPicker <nico1a@naver.com>
2024-10-21 11:46:45 +0900

    회원가입 제거

--- src/main/java/com/hello/ecommerceorderplatform/BeforeDataInit.java
+++ src/main/java/com/hello/ecommerceorderplatform/BeforeDataInit.java
@@ -3,10 +3,10 @@ package com.hello.ecommerceorderplatform;
 
 import com.hello.ecommerceorderplatform.item.domain.Item;
 import com.hello.ecommerceorderplatform.item.repository.ItemRepository;
-import com.hello.ecommerceorderplatform.user.domain.User;
-import com.hello.ecommerceorderplatform.user.domain.UserRoleEnum;
 import com.hello.ecommerceorderplatform.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 import java.util.Random;
 
@@ -16,33 +16,8 @@ public class BeforeDataInit {
     private final ItemRepository itemRepository;
 
 
-    // @EventListener(ApplicationReadyEvent.class)
+    @EventListener(ApplicationReadyEvent.class)
     public void save() {
-
-        userRepository.save(User.builder()
-                .username("admin")
-                .password("socom!23")
-                .phoneNumber("010-1234-1234")
-                .email("nico1a@naver.com")
-                .address("Seoul")
-                .role(UserRoleEnum.ADMIN)
-                .build());
-        userRepository.save(User.builder()
-                .username("userA")
-                .password("socom!23")
-                .phoneNumber("010-1234-1234")
-                .email("abc@naver.com")
-                .address("Seoul")
-                .role(UserRoleEnum.USER)
-                .build());
-        userRepository.save(User.builder()
-                .username("userB")
-                .password("socom!23")
-                .phoneNumber("010-1234-1234")
-                .email("abcd@naver.com")
-                .address("Seoul")
-                .role(UserRoleEnum.USER)
-                .build());
 
 
         for (int i = 1; i <= 100; i++) {
