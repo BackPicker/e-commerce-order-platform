fec3bf7b074d9be8c220547f4e8d6fa2e6a86478
BackPicker <nico1a@naver.com>
2024-10-22 23:05:31 +0900

    의존성 주입

--- src/main/java/com/hello/ecommerceorderplatform/ECommerceOrderPlatformApplication.java
+++ src/main/java/com/hello/ecommerceorderplatform/ECommerceOrderPlatformApplication.java
@@ -4,9 +4,15 @@ import com.hello.ecommerceorderplatform.item.repository.ItemRepository;
 import com.hello.ecommerceorderplatform.user.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableCaching
+@EnableJpaAuditing
+@EnableScheduling
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @SpringBootApplication
 public class ECommerceOrderPlatformApplication {
--- src/main/java/com/hello/ecommerceorderplatform/config/RedisCacheConfig.java
+++ src/main/java/com/hello/ecommerceorderplatform/config/RedisCacheConfig.java
@@ -11,16 +11,17 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.time.Duration;
 
 @Configuration
-@EnableCaching
 public class RedisCacheConfig {
 
     @Value("${spring.data.redis.host}")
