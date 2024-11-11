1ab48f40be85ecae1c0f5d88250017c97ee085cc
BackPicker <nico1a@naver.com>
2024-11-11 11:13:19 +0900

    feat: Redis에 있는 재고 수량을 주기적으로 업데이트

--- item-service/src/main/java/com/back/itemservice/ItemServiceApplication.java
+++ item-service/src/main/java/com/back/itemservice/ItemServiceApplication.java
@@ -3,8 +3,11 @@ package com.back.itemservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+@EnableJpaAuditing
+@EnableScheduling
 @EnableFeignClients
 @SpringBootApplication
 public class ItemServiceApplication {
