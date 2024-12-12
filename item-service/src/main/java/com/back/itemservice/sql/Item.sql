497f3d0ac4f28c4ea420fc7cac2b7fb91cc6c73d
BackPicker <nico1a@naver.com>
2024-12-12 23:44:11 +0900

    perf: ITEM sql문 수정

--- item-service/src/main/java/com/back/itemservice/sql/Item.sql
+++ item-service/src/main/java/com/back/itemservice/sql/Item.sql
@@ -1,5 +1,5 @@
 -- 높은 재귀(반복) 횟수를 허용하도록 설정
-SET SESSION cte_max_recursion_depth = 1000;
+SET SESSION cte_max_recursion_depth = 10000;
 
 -- boards 테이블에 더미 데이터 삽입
 INSERT INTO item (item_name, category, price, quantity, created_at, is_delete)
@@ -7,7 +7,7 @@ WITH RECURSIVE cte (n) AS (SELECT 1
                            UNION ALL
                            SELECT n + 1
                            FROM cte
-                           WHERE n < 1000 -- 생성하고 싶은 더미 데이터의 개수
+                           WHERE n < 10000 -- 생성하고 싶은 더미 데이터의 개수
 )
 SELECT CONCAT('Title', LPAD(n, 7, '0'))                 AS item_name,  -- 'Title' 다음에 7자리 숫자로 구성된 제목 생성
        CONCAT('itemName', LPAD(n, 7, '0')) AS category,
